5e1fb798be6ee27a1e329c00ae5cca25e0352e3c
Ульяна Глухова <Tyuiu.GluhovaUA@outlook.com>
2026-09-12 00:37:45 +0500

    Task2

---
+++ Tyuiu.GluhovaUA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tyuiu.GluhovaUA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GluhovaUA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Ульяна"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
