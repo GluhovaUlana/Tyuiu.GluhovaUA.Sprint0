09175c787db56f5077c4bb35920a8530f34e867f
Ульяна Глухова <Tyuiu.GluhovaUA@outlook.com>
2026-09-12 01:24:34 +0500

    Task3

---
+++ Tyuiu.GluhovaUA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tyuiu.GluhovaUA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.GluhovaUA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
