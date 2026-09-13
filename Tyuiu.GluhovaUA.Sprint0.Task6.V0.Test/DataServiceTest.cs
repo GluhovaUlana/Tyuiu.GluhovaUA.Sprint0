3831d6eefbba029ea5c1e9ec12c48b3361b9bf17
Ульяна Глухова <Tyuiu.GluhovaUA@outlook.com>
2026-09-13 19:55:24 +0500

    Task1

---
+++ Tyuiu.GluhovaUA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.GluhovaUA.Sprint0.Task6.V0.Lib;
+
+using Tyuiu.GluhovaUA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.GluhovaUA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.Subtraction(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.Multiplication(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.Division(numbers);
+            Assert.AreEqual(15, res);
+        }
+    }
+}
---
+++ Tyuiu.GluhovaUA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GluhovaUA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.GluhovaUA.Sprint0.Task6.V0
+{
+    class Progras
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("" = "" + DataService.AdditionArray(arraynums));
+            Console.WriteLine("" = "" + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("" = "" + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+
+        }
+    }
+}
