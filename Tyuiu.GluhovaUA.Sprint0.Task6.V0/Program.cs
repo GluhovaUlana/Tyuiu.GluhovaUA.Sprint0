7cd26cd0b9ef909d6ed01cec5b8ccf0bd214c173
Ульяна Глухова <Tyuiu.GluhovaUA@outlook.com>
2026-09-15 19:28:17 +0500

    Task6

--- Tyuiu.GluhovaUA.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.GluhovaUA.Sprint0.Task6.V0/Program.cs
@@ -11,9 +11,9 @@ namespace Tyuiu.GluhovaUA.Sprint0.Task6.V0
         static void Main(string[] args)
         {
             int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
-            Console.WriteLine("Сумма элементов массива" = " + DataService.AdditionArray(arraynums));
-            Console.WriteLine("Разность элементов массива" = " + DataService.SubtractionArray(arraynums));
-            Console.WriteLine("Произведение элементов массива" = " + DataService.MultiplicationArray(arraynums));
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива =" + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
             Console.ReadKey();
 
         }
---
+++ Tyuiu.GluhovaUA.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Tyuiu.GluhovaUA.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.GluhovaUA.Sprint0.Task7.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнила: Глухова У. А.| ПИНб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("*Выполнила: Глухова У. А.| ПИНб-26-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
+            Console.WriteLine("* одинаковых массивов по длинне.                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine();
+            for (int i = 0; i < arrayNums1.Length; i++) ;
+            {
+                Console.WriteLine(arrayNums1[i]+", ");
+            }
+            Console.WriteLine();
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine();
+            for (int i = 0; i < arrayNums2.Length; i++) ;
+            {
+                Console.WriteLine(arrayNums2[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+        }   
+    }
+}
