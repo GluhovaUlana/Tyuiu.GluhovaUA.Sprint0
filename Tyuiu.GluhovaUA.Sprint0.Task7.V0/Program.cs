16ff05a5e2965b2c985c7892196866ab6c1ebff0
Ульяна Глухова <Tyuiu.GluhovaUA@outlook.com>
2026-09-15 20:02:18 +0500

    Task7

--- Tyuiu.GluhovaUA.Sprint0.Task7.V0/Program.cs
+++ Tyuiu.GluhovaUA.Sprint0.Task7.V0/Program.cs
@@ -23,14 +23,14 @@ namespace Tyuiu.GluhovaUA.Sprint0.Task7.V0
             Console.WriteLine("***************************************************************************");
             int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
             Console.WriteLine();
-            for (int i = 0; i < arrayNums1.Length; i++) ;
+            for (int i = 0; i < arrayNums1.Length; i++)
             {
                 Console.WriteLine(arrayNums1[i]+", ");
             }
             Console.WriteLine();
             int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
             Console.WriteLine();
-            for (int i = 0; i < arrayNums2.Length; i++) ;
+            for (int i = 0; i < arrayNums2.Length; i++)
             {
                 Console.WriteLine(arrayNums2[i] + ", ");
             }
@@ -38,7 +38,20 @@ namespace Tyuiu.GluhovaUA.Sprint0.Task7.V0
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            
+            if (arrayNums1.Length == arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+                Console.WriteLine("Сумма элементовмассива равна:");
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    Console.WriteLine(resultArray[i] + ", ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Исходные массивы имеют разное колличество элементов");
+            }
+            Console.ReadKey();
         }   
     }
 }
